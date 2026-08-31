3a76293da14f5c6a5537ae5992f6f356aa9b9a03
sbtalk71 <sbtalk@yahoo.com>
2026-08-31 15:05:35 +0530

    soap app updated

---
+++ sb-calculator-ws/src/main/java/com/demo/spring/SbCalculatorWsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCalculatorWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCalculatorWsApplication.class, args);
+	}
+
+}
